7c9b289107298d4beb1fe57e1dcbd37807d33522
Ole Martin Bjørndalen <ombdalen@gmail.com>
2017-01-07 19:04:59 +0100

    Updated test_blocklag.py.

--- test_blocklag.py
+++ test_blocklag.py
@@ -3,19 +3,14 @@ import time
 from overdub.deck import Deck
 
 def test_blocklag():
-    def do_blocks(deck, n):
-        for _ in range(n):
-            deck.handle_block()
-
-
     deck = Deck()
-
 
     while True:
         print('Rewind!')
         deck.pos = 0
         deck.mode = 'recording'
-        do_blocks(deck, 43)
+        for i in range(43):
+            deck.update()
 
 
 test_blocklag()

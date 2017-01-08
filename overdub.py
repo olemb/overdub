db25f707e0c5f18a8c2a3878e36d9bdd85574895
Ole Martin Bjørndalen <ombdalen@gmail.com>
2017-01-08 13:57:12 +0100

    Added proper controls.

--- overdub.py
+++ overdub.py
@@ -26,31 +26,53 @@ class GUI:
         self.root = root = Tk()
         root.title('Overdub')
 
-        label = Label(text='Writing to: ' + self.filename)
-        label['font'] = get_font(size=15)
-        label.pack(side=TOP,padx=10,pady=10)
-
         self.statusbar = StringVar()
         label = Label(textvariable=self.statusbar)
         label['font'] = get_font(size=30)
         label.pack(side=TOP,padx=10,pady=10)
 
-        for (text, command) in [
-            ('Record', self.record),
-            ('Play', self.play),
-            ('Stop', self.stop),
-            ('Undo', self.undo),
-        ]:
-            button = Button(root, text=text, command=command)
-            button['font'] = get_font(size=30)
-            button.pack(side=LEFT)
+        label = Label(text='Writing to: ' + self.filename)
+        label['font'] = get_font(size=15)
+        label.pack(side=TOP,padx=10,pady=10)
+
+        if False:
+            for (text, command) in [
+                    ('Record', self.record),
+                    ('Play', self.play),
+                    ('Stop', self.stop),
+                    ('Undo', self.undo),
+            ]:
+                button = Button(root, text=text, command=command)
+                button['font'] = get_font(size=30)
+                button.pack(side=LEFT)
+
+        self.root.bind("<KeyPress-Return>", lambda event: self.toggle_record())
+        self.root.bind("<KeyPress-space>", lambda event: self.toggle_play())
+        self.root.bind("<KeyPress-BackSpace>", lambda event: self.undo())
+
+        self.skipdist = 0
+        skipdist = 10
+
+        def add_skipdist(skipdist):
+            self.skipdist += skipdist
+
+        # We can't use KeyPress/KeyRelease because of key repeat.
+        self.root.bind("<ButtonPress-1>", lambda event: add_skipdist(-skipdist))
+        self.root.bind("<ButtonRelease-1>", lambda event: add_skipdist(skipdist))
+
+        self.root.bind("<ButtonPress-2>", lambda event: self.toggle_record())
+
+        self.root.bind("<ButtonPress-3>", lambda event: add_skipdist(skipdist))
+        self.root.bind("<ButtonRelease-3>", lambda event: add_skipdist(-skipdist))
 
         self.deck = Deck(blocks)
         self.update_statusbar()
 
     def update(self):
         self.root.update()
+        self.root.update_idletasks()
         self.deck.update()
+        self.skip(self.skipdist)
 
     def mainloop(self):
         try:
@@ -60,17 +82,37 @@ class GUI:
             return
 
     def play(self):
-        self.deck.pos = 0
         self.deck.mode = 'playing'
 
     def record(self):
         self.undo_blocks = self.deck.blocks.copy()
-        self.deck.pos = 0
         self.deck.mode = 'recording'
 
     def stop(self):
-        self.deck.pos = 0
         self.deck.mode = 'stopped'
+
+    def toggle_play(self):
+        if self.deck.mode == 'stopped':
+            self.play()
+        else:
+            self.stop()
+
+    def toggle_record(self):
+        if self.deck.mode == 'recording':
+            self.play()
+        else:
+            self.record()
+
+    def skip(self, numblocks=0):
+        if numblocks == 0:
+            return
+
+        if self.deck.mode == 'recording':
+            self.deck.mode = 'playing'
+
+        self.deck.pos += numblocks
+        if self.deck.pos < 0:
+            self.deck.pos = 0
 
     def undo(self):
         if self.deck.mode == 'recording':
@@ -89,7 +131,7 @@ class GUI:
         if self.undo_blocks is not None:
             text += '(undo possible)'
         self.statusbar.set(text)
-        self.root.after(50, self.update_statusbar)
+        self.root.after(10, self.update_statusbar)
 
 
 filename = os.path.expanduser('~/Desktop/overdub-out.wav')
--- overdub/deck.py
+++ overdub/deck.py
@@ -58,12 +58,15 @@ class Deck:
         inblock = self.audio_in.read_block()
         outblock = audio.SILENCE
 
+        # print(self.audio_in.latency, self.audio_out.latency)
+
         if self.mode != 'stopped':
             block = play_block(self.blocks, self.pos)
             outblock = audio.add_blocks([outblock, block])
 
         # We need to record after playing back in case the block is
         # recorded at the same position as playback.
+        self.blocklag = 5
         if self.mode == 'recording':
             record_block(self.blocks, self.pos - self.blocklag, inblock)
 

21ed80fe1a6a1bb6783f2669e13ae02d5b058a2a
Ole Martin Bjørndalen <ombdalen@gmail.com>
2017-10-07 14:41:03 +0200

    Updated some help texts.

--- overdub/main.py
+++ overdub/main.py
@@ -5,8 +5,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
 
-    arg('--terminal', '-t', action='store_true', help='Run in terminal')
-    arg('--minimalist', '-m', action='store_true', help='Minimalst UI')
+    arg('--terminal', '-t', action='store_true', help='run in terminal')
+    arg('--minimalist', '-m', action='store_true', help='use minimalst UI')
     arg('infile', nargs='?')
 
     return parser.parse_args()

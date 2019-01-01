373e89f24be5341dbe9bf22e96886d86add5446d
Ole Martin Bjørndalen <ombdalen@gmail.com>
2019-01-01 10:05:28 +0900

    Added some dataclasses for views and commands.

---
+++ overdub/commands.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Goto:
+    time: float
+
+
+@dataclass(frozen=True)
+class Skip:
+    time: float
+
+
+@dataclass(frozen=True)
+class Record:
+    pass
+
+
+@dataclass(frozen=True)
+class Play:
+    pass
+
+
+@dataclass(frozen=True)
+class Stop:
+    pass
+
+
+@dataclass(frozen=True)
+class TogglePlay:
+    pass
+
+
+@dataclass(frozen=True)
+class ToggleRecord:
+    pass
